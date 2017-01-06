0ba5c579fa0a24bcd56104b06f0c8291c490878e
Saulo Camposi <saulocamposi@gmail.com>
2017-01-06 03:11:58 -0200

    Change homecontrollertest

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+
+  #user = FactoryGirl.create(:user)
+  #user.confirmed_at = Time.now
+  #user.save
+
   test "should get index" do
     get :index
     assert_response :success
